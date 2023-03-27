6bab5758973c6121db2a972bded8299c4e93fb49
anikkarmakar3 <anikkarmakar3@gmail.com>
2023-03-28 00:31:28 +0300

    ghgh

--- app/src/main/java/com/example/azurework/BlobAdapter.kt
+++ app/src/main/java/com/example/azurework/BlobAdapter.kt
@@ -1,23 +1,19 @@
 package com.example.azurework
 
+import android.app.ProgressDialog
 import android.content.Context
-
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
-import com.microsoft.azure.storage.CloudStorageAccount
-import com.microsoft.azure.storage.blob.CloudBlob
-import com.microsoft.azure.storage.blob.ListBlobItem
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import java.io.FileOutputStream
 
 class BlobAdapter(val items : List<FileModel>, val context: Context) : RecyclerView.Adapter<MyViewHolder>() {
     private var mainActivityObject= MainActivity()
+    private var mainActivityObject2= MainActivity2()
+    lateinit var progressBar: ProgressDialog
+    private val progressBarStatus = 0
     val connectionString =
         "DefaultEndpointsProtocol=https;AccountName=anikkarmakar;AccountKey=dN+fVtZihBvdf0QSSUVl68gHjqcVwsHFTsIfelmsEoeatvx8e2FwTwMEk9WLzzhL7seVCyS2zDGU+AStDBf46A==;EndpointSuffix=core.windows.net"
 
@@ -39,8 +35,8 @@ class BlobAdapter(val items : List<FileModel>, val context: Context) : RecyclerV
         holder.blobItemView.setOnClickListener {
 
             try {
-                mainActivityObject.downloadF(blobItem.blobName.toString())
-
+                /*mainActivityObject.downloadF(blobItem.blobName.toString())*/
+                downloadWithprogressBar(blobItem.blobName.toString(),blobItem.blobSize.toLong())
                 Toast.makeText(context,"success fully download ${blobItem.blobName}",Toast.LENGTH_LONG).show()
             }catch (e:Exception){
                 e.printStackTrace()
@@ -50,7 +46,72 @@ class BlobAdapter(val items : List<FileModel>, val context: Context) : RecyclerV
         }
     }
 
+
+
+
+    fun downloadWithprogressBar(getBlobName:String,fileSize:Long){
+        try {
+            progressBar = ProgressDialog(context)
+            progressBar.setCancelable(true)
+            progressBar.setMessage("File downloading ...")
+            progressBar.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL)
+            progressBar.progress = 0
+            progressBar.max = 100
+            progressBar.show()
+
+
+
+        }catch (e:Exception){
+            e.printStackTrace()
+        }
+
+
+        while (progressBarStatus<100){
+            val progressBarStatus = doOperation(fileSize.toInt())
+
+            try {
+                Thread.sleep(1000)
+                progressBar.setProgress(progressBarStatus);
+            } catch (e: InterruptedException) {
+                e.printStackTrace()
+            }
+
+            if (progressBarStatus >= 100000) {
+                // sleeping for 1 second after operation completed
+                try {
+                    Thread.sleep(1000);
+                } catch (e:InterruptedException) {
+                    e.printStackTrace();
+                }
+                // close the progress bar dialog
+                progressBar.dismiss();
+            }
+        }
+    }
+
+    fun doOperation(fileSize:Int):Int{
+
+        var i=0
+        while ( i < fileSize) {
+            i++
+            /*if (fileSize == 1000) {
+                return 10
+            } else if (fileSize == 2000) {
+                return 20
+            } else if (fileSize == 3000) {
+                return 30
+            } else if (fileSize == 4000) {
+                return 40 // you can add more else if
+            }
+             else {
+                   return 100;
+               }*/
+        }//end of while
+        return i
+    }
+
 }
+
 
 class MyViewHolder (view: View) : RecyclerView.ViewHolder(view) {
     val blobItemView:TextView= view.findViewById<TextView>(R.id.blob_list_item)
--- app/src/main/java/com/example/azurework/MainActivity2.kt
+++ app/src/main/java/com/example/azurework/MainActivity2.kt
@@ -1,39 +1,78 @@
 package com.example.azurework
 
+import android.app.ProgressDialog
 import android.os.Bundle
-import android.os.Environment
-import android.os.FileUtils.ProgressListener
 import android.view.View
-import android.widget.ProgressBar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.microsoft.azure.storage.CloudStorageAccount
-import com.microsoft.azure.storage.blob.BlobRequestOptions
-import java.io.File
 
 
 class MainActivity2 : AppCompatActivity() {
     private lateinit var adapter:BlobAdapter
     val newsarray = ArrayList<FileModel>()
-    lateinit var progressBar: ProgressBar
+    lateinit var progressBar: ProgressDialog
+    private val progressBarStatus = 0
+    private val fileSize: Long = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
 
-        progressBar= findViewById(R.id.progressBar2)
-        progressBar.visibility= View.VISIBLE
+        /*progressBar= findViewById(R.id.progressBar2)
+        progressBar.visibility= View.VISIBLE*/
+
         val newsarray = this.intent.extras?.getParcelableArrayList<FileModel>("ArrayData")
         val recyclerview = findViewById<RecyclerView>(R.id.rv_blob_list)
         recyclerview.layoutManager= LinearLayoutManager(this)
-
         adapter = BlobAdapter(newsarray!!,this)
         recyclerview.adapter= adapter
         adapter.notifyDataSetChanged()
+
+
     }
 
-    fun downloadWithprogressBar(getBlobName:String){
-        val storageAccount = CloudStorageAccount.parse("DefaultEndpointsProtocol=https;AccountName=anikkarmakar;AccountKey=dN+fVtZihBvdf0QSSUVl68gHjqcVwsHFTsIfelmsEoeatvx8e2FwTwMEk9WLzzhL7seVCyS2zDGU+AStDBf46A==;EndpointSuffix=core.windows.net")
+    fun downloadWithprogressBar(getBlobName:String,fileSize:Long){
+        try {
+            progressBar = ProgressDialog(this@MainActivity2)
+            progressBar.setCancelable(true)
+            progressBar.setMessage("File downloading ...")
+            progressBar.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL)
+            progressBar.progress = 0
+            progressBar.max = 100
+            progressBar.show()
+
+        }catch (e:Exception){
+            e.printStackTrace()
+        }
+
+
+        while (progressBarStatus<100){
+            val progressBarStatus = doOperation(fileSize.toInt())
+
+            try {
+                /*Thread.sleep(1000)*/
+                progressBar.setProgress(progressBarStatus);
+            } catch (e: InterruptedException) {
+                e.printStackTrace()
+            }
+
+            if (progressBarStatus >= 100) {
+                // sleeping for 1 second after operation completed
+                try {
+                    Thread.sleep(1000);
+                } catch (e:InterruptedException) {
+                    e.printStackTrace();
+                }
+                // close the progress bar dialog
+                progressBar.dismiss();
+            }
+        }
+    }
+
+
+
+
+        /*val storageAccount = CloudStorageAccount.parse("DefaultEndpointsProtocol=https;AccountName=anikkarmakar;AccountKey=dN+fVtZihBvdf0QSSUVl68gHjqcVwsHFTsIfelmsEoeatvx8e2FwTwMEk9WLzzhL7seVCyS2zDGU+AStDBf46A==;EndpointSuffix=core.windows.net")
 
         val blobClient = storageAccount.createCloudBlobClient()
 
@@ -41,13 +80,28 @@ class MainActivity2 : AppCompatActivity() {
 
         val blob = container.getBlockBlobReference(getBlobName)
 
-        val localFile = File(Environment.getExternalStorageDirectory().absoluteFile.path )
+        val localFile = File(Environment.getExternalStorageDirectory().absoluteFile.path )*/
         /*blob.downloadToFile(localFile.absolutePath,null, BlobRequestOptions(),)
 
         progressBar.setVisibility(View.GONE)*/
-    }
 
-    fun test(){
+    fun doOperation(fileSize:Int):Int{
+        while (fileSize <= 10000) {
+            fileSize+1
+            if (fileSize == 1000) {
+                return 10
+            } else if (fileSize == 2000) {
+                return 20
+            } else if (fileSize == 3000) {
+                return 30
+            } else if (fileSize == 4000) {
+                return 40 // you can add more else if
+            }
+            /* else {
+                   return 100;
+               }*/
 
+        }//end of while
+        return 100
     }
 }
